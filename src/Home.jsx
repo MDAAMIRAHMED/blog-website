0068beac918d0c8cbe543514448b05a5447d47a0
Mohammed Aamir Ahmed <mohammedaamirahmed5768@gmail.com>
2025-08-17 14:22:39 +0530

    click events

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 function Home() {
+  const handleClick = (e) => {
+    console.log("Click me", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello", name, e.target);
+  };
   return (
     <div className="home">
       <h2>HomePage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("John", e)}>
+        Click me again
+      </button>
     </div>
   );
 }
